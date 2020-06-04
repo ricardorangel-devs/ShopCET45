11d746cf675050f8c1483a692c74e058b0efaba7
Ricardo Rangel <Ricardo.Miranda.Rangel@formandos.cinel.pt>
2020-06-04 11:56:37 +0100

    Create database and products controller.

---
+++ ShopCET45.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopCET45.Web.Data.Entities;
+
+namespace ShopCET45.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ ShopCET45.Web/Data/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopCET45.Web.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+
+        public string Name { get; set; }
+
+
+        public decimal Price { get; set; }
+
+        [Display(Name ="Image")]
+        public string ImageUrl { get; set; }
+
+
+        [Display(Name ="Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Available ?")]
+        public bool IsAvailable { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
